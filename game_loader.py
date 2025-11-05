7bb767678315ca8b14073a40dcfa86f456f02463
HatimAsseffar <htmasf20@gmail.com>
2025-11-05 14:41:57 +0100

    Loader Script

---
+++ game_loader.py
@@ -0,0 +1,9 @@
+# game_loader.py
+from adpt.game_state_loader_adapter import restore_from_payload
+
+class GameStateLoader:
+    @staticmethod
+    def restore(loaded_data):
+        if not loaded_data:
+            return None
+        return restore_from_payload(loaded_data)
