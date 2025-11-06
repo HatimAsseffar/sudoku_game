157804ddf10dbf0ded3a89375825612740793fba
HatimAsseffar <htmasf20@gmail.com>
2025-11-06 12:56:46 +0100

    Save Adapter

--- adpt/autosave_adapter.py
+++ adpt/autosave_adapter.py
@@ -1,8 +1,8 @@
 import json
 import os
 from typing import Callable, Optional, Dict
-from pl.pl_autosave import deserialize, serialize                                         
-
+from pl.pl_autosave import deserialize, serialize
+#
 class AutosaveAdapter:
     def __init__(self, save_path: str = "sudoku_autosave.json"):
         self.save_path = save_path
